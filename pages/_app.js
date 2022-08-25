f67a705e3456faed53784d22670cfbbe2456f9bc
Bismvrckz <ahmadfayruzsyamil33@gmail.com>
2022-08-25 20:29:51 +0700

    first commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,27 @@
-import '../styles/globals.css'
+import "../styles/globals.css";
+import Head from "next/head";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <div className="font-[Poppins]">
+      <Head>
+        <title>Bismarckz</title>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap"
+          rel="stylesheet"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          href="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSdpL-3B2esy2Q9r8TTVdYVECEdW5KE-_pL4q9os6ej7a20louanTZsZu4Oq31UBd88Pxk&usqp=CAU"
+          sizes="32x32"
+        />
+      </Head>
+      <Component {...pageProps} />
+    </div>
+  );
 }
 
-export default MyApp
+export default MyApp;
---
+++ pages/navbar/index.js
@@ -0,0 +1,148 @@
+import React from "react";
+import Link from "next/link";
+
+function Navbar() {
+  return (
+    <>
+      <div className="relative w-[100vw] h-[7vh] flex items-center">
+        <div className="bg-transparent z-[2]">
+          <Link href="/">
+            <a className="font-[600] text-[1.5rem]">Bismarckz</a>
+          </Link>
+        </div>
+        <div className="bg-gray-700 w-[100vw] h-[7vh] overflow-hidden absolute z-[1]">
+          <div className="rotate-45 mt-[-100vh] ml-[-70vw] tablet:mt-[-80vh] desktop:mt-[-120vh] desktop:ml-[-10vw]  z-[1]">
+            <div className="bg-yellow-500 h-[15px]"></div>
+            <div className="bg-black h-[15px]"></div>
+            <div className="bg-yellow-500 h-[15px]"></div>
+            <div className="bg-black h-[15px]"></div>
+            <div className="bg-yellow-500 h-[15px]"></div>
+            <div className="bg-black h-[15px]"></div>
+            <div className="bg-yellow-500 h-[15px]"></div>
+            <div className="bg-black h-[15px]"></div>
+            <div className="bg-yellow-500 h-[15px]"></div>
+            <div className="bg-black h-[15px]"></div>
+            <div className="bg-yellow-500 h-[15px]"></div>
+            <div className="bg-black h-[15px]"></div>
+            <div className="bg-yellow-500 h-[15px]"></div>
+            <div className="bg-black h-[15px]"></div>
+            <div className="bg-yellow-500 h-[15px]"></div>
+            <div className="bg-black h-[15px]"></div>
+            <div className="bg-yellow-500 h-[15px]"></div>
+            <div className="bg-black h-[15px]"></div>
+            <div className="bg-yellow-500 h-[15px]"></div>
+            <div className="bg-black h-[15px]"></div>
+            <div className="bg-yellow-500 h-[15px]"></div>
+            <div className="bg-black h-[15px]"></div>
+            <div className="bg-yellow-500 h-[15px]"></div>
+            <div className="bg-black h-[15px]"></div>
+            <div className="bg-yellow-500 h-[15px]"></div>
+            <div className="bg-black h-[15px]"></div>
+            <div className="bg-yellow-500 h-[15px]"></div>
+            <div className="bg-black h-[15px]"></div>
+            <div className="bg-yellow-500 h-[15px]"></div>
+            <div className="bg-black h-[15px]"></div>
+            <div className="bg-yellow-500 h-[15px]"></div>
+            <div className="bg-black h-[15px]"></div>
+            <div className="bg-yellow-500 h-[15px]"></div>
+            <div className="bg-black h-[15px]"></div>
+            <div className="bg-yellow-500 h-[15px]"></div>
+            <div className="bg-black h-[15px]"></div>
+            <div className="bg-yellow-500 h-[15px]"></div>
+            <div className="bg-black h-[15px]"></div>
+            <div className="bg-yellow-500 h-[15px]"></div>
+            <div className="bg-black h-[15px]"></div>
+            <div className="bg-yellow-500 h-[15px]"></div>
+            <div className="bg-black h-[15px]"></div>
+            <div className="bg-yellow-500 h-[15px]"></div>
+            <div className="bg-black h-[15px]"></div>
+            <div className="bg-yellow-500 h-[15px]"></div>
+            <div className="bg-black h-[15px]"></div>
+            <div className="bg-yellow-500 h-[15px]"></div>
+            <div className="bg-black h-[15px]"></div>
+            <div className="bg-yellow-500 h-[15px]"></div>
+            <div className="bg-black h-[15px]"></div>
+            <div className="bg-yellow-500 h-[15px]"></div>
+            <div className="bg-black h-[15px]"></div>
+            <div className="bg-yellow-500 h-[15px]"></div>
+            <div className="bg-black h-[15px]"></div>
+            <div className="bg-yellow-500 h-[15px]"></div>
+            <div className="bg-black h-[15px]"></div>
+            <div className="bg-yellow-500 h-[15px]"></div>
+            <div className="bg-black h-[15px]"></div>
+            <div className="bg-yellow-500 h-[15px]"></div>
+            <div className="bg-black h-[15px]"></div>
+            <div className="bg-yellow-500 h-[15px]"></div>
+            <div className="bg-black h-[15px]"></div>
+            <div className="bg-yellow-500 h-[15px]"></div>
+            <div className="bg-black h-[15px]"></div>
+            <div className="bg-yellow-500 h-[15px]"></div>
+            <div className="bg-black h-[15px]"></div>
+            <div className="bg-yellow-500 h-[15px]"></div>
+            <div className="bg-black h-[15px]"></div>
+            <div className="bg-yellow-500 h-[15px]"></div>
+            <div className="bg-black h-[15px]"></div>
+            <div className="bg-yellow-500 h-[15px]"></div>
+            <div className="bg-black h-[15px]"></div>
+            <div className="bg-yellow-500 h-[15px]"></div>
+            <div className="bg-black h-[15px]"></div>
+            <div className="bg-yellow-500 h-[15px]"></div>
+            <div className="bg-black h-[15px]"></div>
+            <div className="bg-yellow-500 h-[15px]"></div>
+            <div className="bg-black h-[15px]"></div>
+            <div className="bg-yellow-500 h-[15px]"></div>
+            <div className="bg-black h-[15px]"></div>
+            <div className="bg-yellow-500 h-[15px]"></div>
+            <div className="bg-black h-[15px]"></div>
+            <div className="bg-yellow-500 h-[15px]"></div>
+            <div className="bg-black h-[15px]"></div>
+            <div className="bg-yellow-500 h-[15px]"></div>
+            <div className="bg-black h-[15px]"></div>
+            <div className="bg-yellow-500 h-[15px]"></div>
+            <div className="bg-black h-[15px]"></div>
+            <div className="bg-yellow-500 h-[15px]"></div>
+            <div className="bg-black h-[15px]"></div>
+            <div className="bg-yellow-500 h-[15px]"></div>
+            <div className="bg-black h-[15px]"></div>
+            <div className="bg-yellow-500 h-[15px]"></div>
+            <div className="bg-black h-[15px]"></div>
+            <div className="bg-yellow-500 h-[15px]"></div>
+            <div className="bg-black h-[15px]"></div>
+            <div className="bg-yellow-500 h-[15px]"></div>
+            <div className="bg-black h-[15px]"></div>
+            <div className="bg-yellow-500 h-[15px]"></div>
+            <div className="bg-black h-[15px]"></div>
+            <div className="bg-yellow-500 h-[15px]"></div>
+            <div className="bg-black h-[15px]"></div>
+            <div className="bg-yellow-500 h-[15px]"></div>
+            <div className="bg-black h-[15px]"></div>
+            <div className="bg-yellow-500 h-[15px]"></div>
+            <div className="bg-black h-[15px]"></div>
+            <div className="bg-yellow-500 h-[15px]"></div>
+            <div className="bg-black h-[15px]"></div>
+            <div className="bg-yellow-500 h-[15px]"></div>
+            <div className="bg-black h-[15px]"></div>
+            <div className="bg-yellow-500 h-[15px]"></div>
+            <div className="bg-black h-[15px]"></div>
+            <div className="bg-yellow-500 h-[15px]"></div>
+            <div className="bg-black h-[15px]"></div>
+            <div className="bg-yellow-500 h-[15px]"></div>
+            <div className="bg-black h-[15px]"></div>
+            <div className="bg-yellow-500 h-[15px]"></div>
+            <div className="bg-black h-[15px]"></div>
+            <div className="bg-yellow-500 h-[15px]"></div>
+            <div className="bg-black h-[15px]"></div>
+            <div className="bg-yellow-500 h-[15px]"></div>
+            <div className="bg-black h-[15px]"></div>
+            <div className="bg-yellow-500 h-[15px]"></div>
+            <div className="bg-black h-[15px]"></div>
+            <div className="bg-yellow-500 h-[15px]"></div>
+            <div className="bg-black h-[15px]"></div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default Navbar;
