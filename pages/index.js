fd891fa42cea705951d53b89d6a3082cc63145b0
Bismvrckz <ahmadfayruzsyamil33@gmail.com>
2022-08-25 20:48:22 +0700

    navbar fix

--- pages/index.js
+++ pages/index.js
@@ -3,21 +3,41 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="bg-gray-800 overflow-auto w-[100vw] h-[100vh]">
+    <div className="bg-gray-800 w-[100vw] h-[100vh] flex flex-col">
       <Navbar />
-      <div className="w-[90vh] h-[90vh]">
-        <Image
-          alt=""
-          unoptimized
-          width={500}
-          height={500}
-          layout="responsive"
-          style={{ borderRadius: "50%" }}
-          src="https://as1.ftcdn.net/v2/jpg/02/09/53/88/1000_F_209538856_67eeD1CrqajhdkO3mcQ5W5yVOaLYW2FS.jpg"
-          loader={() => {
-            return "https://as1.ftcdn.net/v2/jpg/02/09/53/88/1000_F_209538856_67eeD1CrqajhdkO3mcQ5W5yVOaLYW2FS.jpg";
-          }}
-        />
+      <div className="overflow-auto h-[93vh]">
+        <div className="w-[100%] h-[100%] bg-gray-600">
+          <div className="w-[90vh] h-[90vh]">
+            <Image
+              alt=""
+              unoptimized
+              width={500}
+              height={500}
+              layout="responsive"
+              style={{ borderRadius: "50%" }}
+              src="https://as1.ftcdn.net/v2/jpg/02/09/53/88/1000_F_209538856_67eeD1CrqajhdkO3mcQ5W5yVOaLYW2FS.jpg"
+              loader={() => {
+                return "https://as1.ftcdn.net/v2/jpg/02/09/53/88/1000_F_209538856_67eeD1CrqajhdkO3mcQ5W5yVOaLYW2FS.jpg";
+              }}
+            />
+          </div>
+        </div>
+        <div className="w-[100%] h-[100%] bg-gray-600">
+          <div className="w-[90vh] h-[90vh]">
+            <Image
+              alt=""
+              unoptimized
+              width={500}
+              height={500}
+              layout="responsive"
+              style={{ borderRadius: "50%" }}
+              src="https://as1.ftcdn.net/v2/jpg/02/09/53/88/1000_F_209538856_67eeD1CrqajhdkO3mcQ5W5yVOaLYW2FS.jpg"
+              loader={() => {
+                return "https://as1.ftcdn.net/v2/jpg/02/09/53/88/1000_F_209538856_67eeD1CrqajhdkO3mcQ5W5yVOaLYW2FS.jpg";
+              }}
+            />
+          </div>
+        </div>
       </div>
     </div>
   );
